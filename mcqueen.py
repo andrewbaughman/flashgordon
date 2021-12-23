d54e7285f08863c2c8d8b03c7cc15f8e060ca925
Andrew Baughman <andrewbaughman@icloud.com>
2021-12-23 13:18:32 -0500

    Moved entirely to mcqueen, not quite built for multiprocessing

--- mcqueen.py
+++ mcqueen.py
@@ -7,6 +7,8 @@ from csv import writer
 from itertools import islice
 import psycopg2
 import tldextract
+from multiprocessing import Process
+
 # import requests_cache DNS Caching decreased performance slightly, but might be helpful in the future. 
 
 
@@ -16,16 +18,15 @@ def append_list_as_row(file_name, list_of_elem):
 		csv_writer = writer(write_obj)
 		csv_writer.writerow(list_of_elem)
 
-def insert_link(link):
-	""" insert a new link into the Link table """
-	sql = """INSERT INTO razorback_link(point_b, visited, point_a_id) VALUES(%s,%s,%s) RETURNING id;"""
+def run_sql(sql, params=None):
+	""" Run any sql string on the lightning_db database"""
 	conn = None
-	id = None
+	ret = None
 	try:
 		conn = psycopg2.connect(dbname="lightning_db", user="flash", password="password")
 		cur = conn.cursor()
-		cur.execute(sql, link)
-		id = cur.fetchone()[0]
+		cur.execute(sql, params)
+		ret = cur.fetchone()
 		conn.commit()
 		cur.close()
 	except (Exception, psycopg2.DatabaseError) as error:
@@ -33,6 +34,7 @@ def insert_link(link):
 	finally:
 		if conn is not None:
 			conn.close()
+	return ret
 
 def insert_link_list(link_list):
 	"""
@@ -106,96 +108,8 @@ def parse_response(link, response):
 def save_data(link, new_links, content):
 	print('Saving data...')
 	insert_link_list(new_links)
-	
-	""" update a link in the Link table """
-	sql = """UPDATE razorback_link SET visited=True, content=%s WHERE point_b=%s RETURNING id;"""
-	conn = None
-	id = None
-	try:
-		conn = psycopg2.connect(dbname="lightning_db", user="flash", password="password")
-		cur = conn.cursor()
-		cur.execute(sql, [content, link['point_b']])
-		id = cur.fetchone()[0]
-		conn.commit()
-		cur.close()
-	except (Exception, psycopg2.DatabaseError) as error:
-		print(error)
-	finally:
-		if conn is not None:
-			conn.close()
+	run_sql("""UPDATE razorback_link SET visited=True, content=%s WHERE point_b=%s RETURNING id;""", [content, link['point_b']])
 	print('Done saving data.')
-
-def get_count():
-	""" get count of links in the Link table """
-	sql = """SELECT COUNT(*) FROM razorback_link;"""
-	conn = None
-	count = None
-	try:
-		conn = psycopg2.connect(dbname="lightning_db", user="flash", password="password")
-		cur = conn.cursor()
-		cur.execute(sql)
-		count = cur.fetchone()[0]
-		conn.commit()
-		cur.close()
-	except (Exception, psycopg2.DatabaseError) as error:
-		print(error)
-	finally:
-		if conn is not None:
-			conn.close()
-	return count
-
-def count_unvisited():
-	""" get count of unvisited links in the Link table """
-	sql = """SELECT COUNT(*) FROM razorback_link WHERE visited=False;"""
-	conn = None
-	count = None
-	try:
-		conn = psycopg2.connect(dbname="lightning_db", user="flash", password="password")
-		cur = conn.cursor()
-		cur.execute(sql)
-		count = cur.fetchone()[0]
-		conn.commit()
-		cur.close()
-	except (Exception, psycopg2.DatabaseError) as error:
-		print(error)
-	finally:
-		if conn is not None:
-			conn.close()
-	return count
-
-def first_unvisited():
-	""" get first link in the Link table """
-	sql = """SELECT * FROM razorback_link WHERE visited=False FETCH FIRST ROW ONLY;"""
-	conn = None
-	id = None
-	link = None
-	try:
-		conn = psycopg2.connect(dbname="lightning_db", user="flash", password="password")
-		cur = conn.cursor()
-		cur.execute(sql)
-		id = cur.fetchone()[0]
-		conn.commit()
-		cur.close()
-	except (Exception, psycopg2.DatabaseError) as error:
-		print(error)
-	finally:
-		if conn is not None:
-			conn.close()
-
-	try:
-		sql = """SELECT * FROM razorback_link WHERE id=%s;"""
-		conn = psycopg2.connect(dbname="lightning_db", user="flash", password="password")
-		cur = conn.cursor()
-		cur.execute(sql, [id])
-		link = cur.fetchone()
-		conn.commit()
-		cur.close()
-	except (Exception, psycopg2.DatabaseError) as error:
-		print(error)
-	finally:
-		if conn is not None:
-			conn.close()
-	return link
 
 def request_page(link):
 	signal.signal(signal.SIGALRM, alarm_handler)
@@ -208,35 +122,60 @@ def request_page(link):
 		signal.alarm(0)
 	except TimeOutException as ex:
 		print(ex)
-		""" update a link in the Link table """
-		sql = """UPDATE razorback_link SET visited=True WHERE point_b=%s RETURNING id;"""
-		conn = None
-		id = None
-		try:
-			conn = psycopg2.connect(dbname="lightning_db", user="flash", password="password")
-			cur = conn.cursor()
-			cur.execute(sql, [link['point_b']])
-			id = cur.fetchone()[0]
-			conn.commit()
-			cur.close()
-		except (Exception, psycopg2.DatabaseError) as error:
-			print(error)
-		finally:
-			if conn is not None:
-				conn.close()
-		return
+		run_sql("""UPDATE razorback_link SET visited=True WHERE point_b=%s RETURNING id;""", [link['point_b']])
 	except Exception as e:
 		signal.alarm(0)
 		print(str(e))
 		return
 
 
+
+def main_loop():
+	request_succeeded = False
+	measurements = []
+	loop_start = time.time()
+	start = time.time()
+	link = run_sql("""SELECT * FROM razorback_link WHERE visited=False FETCH FIRST ROW ONLY;""")
+	link = {
+		'id': link[0],
+		'point_b': link[1],
+		'visited': link[2],
+		'point_a_id': link[3],
+		'content': link[4]
+	}
+	measurements.append(['model filter', {'code': 'Link.objects.filter(visited=False).first()'}, time.time() - start])
+	if link:
+		try:
+			url = link['point_b']
+			if not link['visited']:
+				start = time.time()
+				response = request_page(link)
+				measurements.append(['request_page', {'destination': link['point_b'], 'source': link['point_a_id']}, time.time() - start])
+				if response.ok:
+					request_succeeded = True
+				start = time.time()
+				new_links, content = parse_response(link, response)
+				measurements.append(['parse_response', {'link_object': link, 'new_links': len(new_links), 'content_length': len(content)}, time.time() - start])
+				start = time.time()
+				save_data(link, new_links, content)
+				measurements.append(['save_data', {'link_object': link, 'new_links': len(new_links), 'content_length': len(content)}, time.time() - start])
+			else:
+				print("link " + str(link['id']) + " was already visited. Skipping...")
+		except Exception as e:
+			break_check = run_sql("""SELECT COUNT(*) FROM razorback_link WHERE visited=False;""")[0]
+	else:
+		break_check = run_sql("""SELECT COUNT(*) FROM razorback_link WHERE visited=False;""")[0]
+	measurements.append(['loop', {'link_object': link}, time.time() - loop_start])
+	return measurements, request_succeeded
+	
+
+
 def __main__():
-	measurements = [['label', 'context', 'data']]
+	append_list_as_row('flashgordon_analytics.csv', ['label', 'context', 'data'])
 	requests_attempted = 0
 	requests_succeeded = 0
-
-	if (get_count() == 0):
+	
+	if (run_sql("""SELECT COUNT(*) FROM razorback_link;""")[0] == 0):
 		x = int(input("how many urls do you want to seed? "))
 		y = x + 1
 		z = 1
@@ -246,65 +185,31 @@ def __main__():
 				url = input("provide seed link #" + str(z) + ":")
 				if not (url[0:7] == 'http://' or url[0:8] == 'https://'):
 					print("NOTE: provide in http:// or https:// form")
-			insert_link([url, False, None])
+			run_sql("""INSERT INTO razorback_link(point_b, visited, point_a_id) VALUES(%s,FALSE,NULL) RETURNING id;""", [url])
 			z = z + 1
 	else:
 		print("resuming crawl.")
 
-	break_check = count_unvisited()
+	break_check = run_sql("""SELECT COUNT(*) FROM razorback_link WHERE visited=False;""")[0]
 	analytics_static_time = time.time()
 	analytics_dynamic_time = time.time()
+	requests_succeeded = 0
+	measurements = []
 	while break_check > 0:
-		loop_start = time.time()
-		start = time.time()
-		link = first_unvisited()
-		link = {
-			'id': link[0],
-			'point_b': link[1],
-			'visited': link[2],
-			'point_a_id': link[3],
-			'content': link[4]
-		}
-		measurements.append(['model filter', {'code': 'Link.objects.filter(visited=False).first()'}, time.time() - start])
-		if link:
-			try:
-				url = link['point_b']
-				if not link['visited']:
-					start = time.time()
-					requests_attempted = requests_attempted + 1
-					response = request_page(link)
-					requests_succeeded = requests_succeeded + 1
-					measurements.append(['request_page', {'destination': link['point_b'], 'source': link['point_a_id']}, time.time() - start])
-					start = time.time()
-					new_links, content = parse_response(link, response)
-					measurements.append(['parse_response', {'link_object': link, 'new_links': len(new_links), 'content_length': len(content)}, time.time() - start])
-					start = time.time()
-					save_data(link, new_links, content)
-					measurements.append(['save_data', {'link_object': link, 'new_links': len(new_links), 'content_length': len(content)}, time.time() - start])
-				else:
-					print("link " + str(link['id']) + " was already visited. Skipping...")
-			except Exception as e:
-				break_check = count_unvisited()
-		else:
-			break_check = count_unvisited()
-		measurements.append(['loop', {'link_object': link}, time.time() - loop_start])
+		ext_measurements, request_succeeded = main_loop()
+		measurements.extend(ext_measurements)
+		if request_succeeded:
+			requests_succeeded = requests_succeeded + 1
+
 		if (time.time() - analytics_dynamic_time) > 10:
 			for measurement in measurements:
 				append_list_as_row('flashgordon_analytics.csv', measurement)
 				measurements = []
-			requests_per_second = requests_attempted / (time.time() - analytics_static_time)
+
+			requests_per_second = requests_succeeded / (time.time() - analytics_static_time)
 			append_list_as_row('flashgordon_analytics.csv', ['requests_per_second', {'requests_attempted': requests_attempted, 'requests_succeeded': requests_succeeded}, requests_per_second])
-			append_list_as_row('flashgordon_analytics.csv', ['request sucess ratio', {'requests_attempted': requests_attempted, 'requests_succeeded': requests_succeeded}, requests_succeeded / requests_attempted])
 			
 			analytics_dynamic_time = time.time()
-
-
-# class Command(BaseCommand):
-# 	measurements = [['label', 'context', 'data']]
-# 	requests_attempted = 0
-# 	requests_succeeded = 0
-# 	session = requests.Session()
-# 	# session = requests_cache.CachedSession() DNS Caching
 
 
 __main__()
